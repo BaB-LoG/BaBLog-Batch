283e151897a24f7e7fcecc05d1cdd57a956fdb6a
yshong1998 <yshong1998@gmail.com>
2025-12-23 19:46:28 +0900

    initial commit

---
+++ src/main/java/com/ssafy/bablog/BaBLogBatchApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.bablog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaBLogBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaBLogBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ssafy/bablog/BaBLogBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.bablog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaBLogBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
